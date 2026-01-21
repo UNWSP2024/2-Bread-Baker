ab11c10fa453069083b6420692afb643afc7fbb0
Bread-Baker <aefafach@students.unwsp.edu>
2026-01-21 11:44:32 -0600

    Wrote Program #1: Personal Information

--- program_1.py
+++ program_1.py
@@ -1,6 +1,12 @@
-def personal_information():
-    print('...')
+# Name: Ariana Fafach
+# Date: 1/21/2026
+# Title: Program #1: Personal Information
 
+def personal_information():
+    print('Name: Susanna Smith') # prints name
+    print('Address: 12345 Main street, Minneapolis, Minnesota 98765') # prints address
+    print('Telephone Number: 123-456-7899') # prints phone number
+    print('College Major: Engineering') # prints college major
 
 # Line which calls the above function.
 personal_information()
